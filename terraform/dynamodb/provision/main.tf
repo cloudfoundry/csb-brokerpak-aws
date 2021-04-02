e6f21d70926c48ffa2f874bace51314349bc0ea4
Ernie Billing <ebilling@pivotal.io>
2021-04-02 14:53:13 -0700

    [#177117613] tabel_name to table_name

--- terraform/dynamodb/provision/main.tf
+++ terraform/dynamodb/provision/main.tf
@@ -14,7 +14,7 @@
 
 resource "aws_dynamodb_table" "this" {
 
-  name             = var.tabel_name
+  name             = var.table_name
   billing_mode     = var.billing_mode
   hash_key         = var.hash_key
   range_key        = var.range_key
