e0374c5233922c4311f86202018054a3a865ab27
Ernie Billing <ebilling@pivotal.io>
2021-04-02 18:26:19 -0700

    [#177205222] added full db rights to bind account so schema creation works

--- terraform/rds/bind/postgresql/main.tf
+++ terraform/rds/bind/postgresql/main.tf
@@ -24,6 +24,15 @@ resource "postgresql_grant" "all_access" {
   depends_on  = [ postgresql_role.new_user ]
   database    = var.db_name
   role        = postgresql_role.new_user.name
+#   schema      = "public"
+  object_type = "database"
+  privileges  = ["ALL"]
+}
+
+resource "postgresql_grant" "table_access" {
+  depends_on  = [ postgresql_role.new_user ]
+  database    = var.db_name
+  role        = postgresql_role.new_user.name
   schema      = "public"
   object_type = "table"
   privileges  = ["ALL"]
