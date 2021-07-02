0c9f51cabc8af76326c9eb7da6399384adf62fd3
George Blue <gblue@pivotal.io>
2021-07-02 11:24:38 +0100

    test: experimental Redis test improved logging
    [#177698509](https://www.pivotaltracker.com/story/show/177698509)

--- acceptance-tests/helpers/http.go
+++ acceptance-tests/helpers/http.go
@@ -1,14 +1,17 @@
 package helpers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func HTTPGet(url string) string {
+	fmt.Fprintf(GinkgoWriter, "HTTP GET: %s\n", url)
 	response, err := http.Get(url)
 	Expect(err).NotTo(HaveOccurred())
 	Expect(response).To(HaveHTTPStatus(http.StatusOK))
@@ -17,10 +20,13 @@ func HTTPGet(url string) string {
 	data, err := io.ReadAll(response.Body)
 	Expect(err).NotTo(HaveOccurred())
 
+	fmt.Fprintf(GinkgoWriter, "Recieved: %s\n", string(data))
 	return string(data)
 }
 
 func HTTPPost(url, data string) {
+	fmt.Fprintf(GinkgoWriter, "HTTP POST: %s\n", url)
+	fmt.Fprintf(GinkgoWriter, "Sending data: %s\n", data)
 	response, err := http.Post(url, "text/html", strings.NewReader(data))
 	Expect(err).NotTo(HaveOccurred())
 	Expect(response).To(HaveHTTPStatus(http.StatusCreated))
