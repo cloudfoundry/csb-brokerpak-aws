0f34f9bda86242f98bdd49674e887776d74b6ba3
Felisia Martini <felisia_martini@hotmail.com>
2021-07-05 15:33:18 +0100

    Remove reference to PE AWS accounts

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -4,10 +4,6 @@ set +x # Hide secrets
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo -e "You must source this script\nsource ${0}" && exit 1
 
-export AWS_ACCESS_KEY_ID=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/cloud service AWS credentials" | jq -r .access_key)
-export AWS_SECRET_ACCESS_KEY=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/cloud service AWS credentials" | jq -r .secret_key)
-export AWS_PAS_VPC_ID=$(lpass show "Shared-CF Platform Engineering/pe-cloud-service-broker/cloud service broker pipeline secrets.yml" | grep aws-vpc-id | cut -d ' ' -f 2)
-
 export SECURITY_USER_NAME=brokeruser
 export SECURITY_USER_PASSWORD=brokeruserpassword
 export DB_HOST=localhost
