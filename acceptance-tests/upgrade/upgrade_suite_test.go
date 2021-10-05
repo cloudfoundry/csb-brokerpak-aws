41ae455decb3ce6462d2f7dd9a5f2e617fb54bcb
Felisia Martini <fmartini@pivotal.io>
2021-10-05 15:06:18 +0100

    chore: fix upgrade tests

--- acceptance-tests/upgrade/upgrade_suite_test.go
+++ acceptance-tests/upgrade/upgrade_suite_test.go
@@ -14,7 +14,7 @@ var releasedBuildDir string
 var brokerName string
 
 func init() {
-	flag.StringVar(&releasedBuildDir, "releasedBuildDir", "/test-broker-update", "location of released version of built broker and brokerpak")
+	flag.StringVar(&releasedBuildDir, "releasedBuildDir", "../../released/", "location of released version of built broker and brokerpak")
 	flag.StringVar(&developmentBuildDir, "developmentBuildDir", "../../", "location of development version of built broker and brokerpak")
 	brokerName = helpers.RandomName("csb")
 }
