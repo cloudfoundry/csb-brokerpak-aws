3ce54df8dda22cad3b61960bd103e823f5262edf
George Blue <gblue@pivotal.io>
2021-10-26 15:17:25 +0100

    test: update s3 app to use AWS Go SDK v2
    [#177720203](https://www.pivotaltracker.com/story/show/177720203)

--- acceptance-tests/apps/s3app/go.mod
+++ acceptance-tests/apps/s3app/go.mod
@@ -3,9 +3,13 @@ module s3app
 go 1.16
 
 require (
-	github.com/aws/aws-sdk-go v1.41.9
+	github.com/aws/aws-sdk-go-v2 v1.10.0
+	github.com/aws/aws-sdk-go-v2/config v1.9.0
+	github.com/aws/aws-sdk-go-v2/credentials v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.17.0
 	github.com/cloudfoundry-community/go-cfenv v1.18.0
 	github.com/gorilla/mux v1.8.0
 	github.com/mitchellh/mapstructure v1.4.2
 	github.com/onsi/gomega v1.10.5 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 )
--- acceptance-tests/apps/s3app/internal/app/app.go
+++ acceptance-tests/apps/s3app/internal/app/app.go
@@ -1,35 +1,45 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"s3app/internal/credentials"
+	appcreds "s3app/internal/credentials"
 
-	"github.com/aws/aws-sdk-go/aws"
-	awscredentials "github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gorilla/mux"
 )
 
-func App(creds credentials.S3Service) *mux.Router {
-	config := aws.NewConfig().
-		WithCredentials(
-			awscredentials.NewStaticCredentials(
-				creds.AccessKeyId,
-				creds.AccessKeySecret,
-				"")).
-		WithRegion(creds.Region)
-
-	session, err := session.NewSession(config)
+func App(creds appcreds.S3Service) *mux.Router {
+	cfg, err := config.LoadDefaultConfig(
+		context.Background(),
+		config.WithCredentialsProvider(
+			aws.NewCredentialsCache(
+				credentials.NewStaticCredentialsProvider(
+					creds.AccessKeyId,
+					creds.AccessKeySecret,
+					"",
+				),
+			),
+		),
+		config.WithRegion(creds.Region),
+	)
 	if err != nil {
 		panic(err)
 	}
+
+	client := s3.NewFromConfig(cfg)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", aliveness).Methods(http.MethodHead, http.MethodGet)
-	r.HandleFunc("/{file_name}", handleUpload(session, creds)).Methods(http.MethodPut)
-	r.HandleFunc("/{file_name}", handleDownload(session, creds)).Methods(http.MethodGet)
-	r.HandleFunc("/{file_name}", handleDelete(session, creds)).Methods(http.MethodDelete)
+	r.HandleFunc("/{file_name}", handleUpload(client, creds)).Methods(http.MethodPut)
+	r.HandleFunc("/{file_name}", handleDownload(client, creds)).Methods(http.MethodGet)
+	r.HandleFunc("/{file_name}", handleDelete(client, creds)).Methods(http.MethodDelete)
 
 	return r
 }
@@ -37,4 +47,10 @@ func App(creds credentials.S3Service) *mux.Router {
 func aliveness(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Handled aliveness test.")
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func fail(w http.ResponseWriter, code int, format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Println(msg)
+	http.Error(w, msg, code)
 }
