97b1bd405cfd8e1c0b480d228ce8b99306c0225e
George Blue <blgm@users.noreply.github.com>
2022-06-09 11:58:05 +0100

    test: s3 acceptance test should trigger HCL update (#326)
    The upgrade test needs to do a "cf update-service" in order to test that
    the upgraded broker replaces the HCL correctly.
    
    [#182285679](https://www.pivotaltracker.com/story/show/182285679)

---
+++ acceptance-tests/upgrade/update_and_upgrade_s3_test.go
@@ -0,0 +1,84 @@
+package upgrade_test
+
+import (
+	"csbbrokerpakaws/acceptance-tests/helpers/apps"
+	"csbbrokerpakaws/acceptance-tests/helpers/brokers"
+	"csbbrokerpakaws/acceptance-tests/helpers/random"
+	"csbbrokerpakaws/acceptance-tests/helpers/services"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("UpgradeS3Test", Label("upgrade"), func() {
+	Context("When upgrading broker version", func() {
+		It("should continue to work", func() {
+			By("pushing latest released broker version")
+			serviceBroker := brokers.Create(
+				brokers.WithPrefix("csb-upgrade"),
+				brokers.WithSourceDir(releasedBuildDir),
+			)
+			defer serviceBroker.Delete()
+
+			By("creating a service")
+			serviceInstance := services.CreateInstance(
+				"csb-aws-s3-bucket",
+				"private",
+				services.WithBroker(serviceBroker),
+			)
+			defer serviceInstance.Delete()
+
+			By("pushing the unstarted app twice")
+			appOne := apps.Push(apps.WithApp(apps.S3))
+			appTwo := apps.Push(apps.WithApp(apps.S3))
+			defer apps.Delete(appOne, appTwo)
+
+			By("binding the apps to the s3 service instance")
+			bindingOne := serviceInstance.Bind(appOne)
+			bindingTwo := serviceInstance.Bind(appTwo)
+			apps.Start(appOne, appTwo)
+
+			By("uploading a blob using the first app")
+			blobNameOne := random.Hexadecimal()
+			blobDataOne := random.Hexadecimal()
+			appOne.PUT(blobDataOne, blobNameOne)
+
+			By("downloading the blob using the second app")
+			got := appTwo.GET(blobNameOne)
+			Expect(got).To(Equal(blobDataOne))
+
+			By("pushing the development version of the broker")
+			serviceBroker.UpdateSourceDir(developmentBuildDir)
+
+			By("re-applying the terraform for service instance")
+			serviceInstance.Update("-p", "public-read")
+
+			By("checking that previously written data is accessible")
+			got = appTwo.GET(blobNameOne)
+			Expect(got).To(Equal(blobDataOne))
+
+			By("deleting bindings created before the upgrade")
+			bindingOne.Unbind()
+			bindingTwo.Unbind()
+
+			By("binding the app to the instance again")
+			serviceInstance.Bind(appOne)
+			serviceInstance.Bind(appTwo)
+			apps.Restage(appOne, appTwo)
+
+			By("checking that previously written data is accessible")
+			got = appTwo.GET(blobNameOne)
+			Expect(got).To(Equal(blobDataOne))
+
+			By("checking that data can still be written and read")
+			blobNameTwo := random.Hexadecimal()
+			blobDataTwo := random.Hexadecimal()
+			appOne.PUT(blobDataTwo, blobNameTwo)
+			got = appTwo.GET(blobNameTwo)
+			Expect(got).To(Equal(blobDataTwo))
+
+			appOne.DELETE(blobNameOne)
+			appOne.DELETE(blobNameTwo)
+		})
+	})
+})
