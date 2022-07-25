cloudfoundry/csb-brokerpak-aws

commit ac03f390718a7ab12867c5e586fb53630b09bbd3

feat: "verify-ca" when connecting to PostgreSQL database (#377)
* feat: "verify-ca" when connecting to PostgreSQL database

- AWS enables TLS on PostgreSQL databases
- The Terraform provider that creates bindings should therefore protect
against "machine in the middle" attacks by validating the CA
- This will require the AWS certificate bundle to have been installed,
but it's a fair bet that folks do this anyway.
- acceptance tests have also been updated to verify TLS via the CA

[#182758757](https://www.pivotaltracker.com/story/show/182758757)

* review comments

- added a switch so that TLS can be turned off, which can be useful in
dev envs
- updated to "verify-full" since this is more secure

* update docs

terraform/postgresql/bind/provider.tf
@@ -19,5 +19,5 @@ provider "postgresql" {
   password  = var.admin_password
   superuser = false
   database  = var.db_name
-  sslmode   = var.use_tls ? "require" : "disable"
+  sslmode   = var.use_tls ? "verify-full" : "prefer"
 }

acceptance-tests/apps/postgresqlapp/internal/app/app.go
@@ -36,7 +36,7 @@ func aliveness(w http.ResponseWriter, r *http.Request) {
 }
 
 func connect(uri string) *sql.DB {
-	db, err := sql.Open("pgx", uri)
+	db, err := sql.Open("pgx", fmt.Sprintf("%s?sslmode=verify-full", uri))
 	if err != nil {
 		log.Fatalf("failed to connect to database: %s", err)
 	}

go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/onsi/ginkgo/v2 v2.1.4
 	github.com/onsi/gomega v1.19.0
 	golang.org/x/tools v0.1.11-0.20220513221640-090b14e8501f
+	gopkg.in/yaml.v3 v3.0.1
 	honnef.co/go/tools v0.3.2
 )
 
@@ -101,7 +102,6 @@ require (
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.3.4 // indirect
 	gorm.io/driver/sqlite v1.3.6 // indirect
 	gorm.io/gorm v1.23.8 // indirect

commit cb158b213216318e8d655106eadfbd3edfe6c2b6

feat(postgresql): new "provider_verify_certificate" (#381)
This fixes a problem introduced by commit ac03f390718a7ab12867c5e586fb53630b09bbd3

AWS PostgreSQL instances will have TLS available by default. We had
previously conflated two concept:
1) Whether the PostgreSQL Terraform provider should attempt to validate
the server certificate. To do this it needs the AWS certificate bundle.
It's typical to have the AWS certificate bundle installed, but some
environments might not. In this case the provider should still connect
over TLS, but not validate the certificate.
2) Whether the server should accept non-TLS connections. This is implied
by the "use_tls" property, but is not implemented correctly yet, as in
all cases the server will currently accept TLS and non-TLS connections.

This PR introduces a new property "provider_verify_certificate" which is
true by default, and can be set to false in order to disable server
certificate checking by the provider.

[#182758757](https://www.pivotaltracker.com/story/show/182758757)

aws-postgresql.yml
@@ -107,7 +107,11 @@ provision:
   - field_name: use_tls
     type: boolean
     details: Use TLS for connection
-    default: true      
+    default: true
+  - field_name: provider_verify_certificate
+    type: boolean
+    details: Whether PostgreSQL Terraform provider should validate the server certificate. The AWS certificate bundle must be installed.
+    default: true
   - field_name: storage_autoscale
     type: boolean
     default: false
@@ -317,6 +321,9 @@ provision:
   - field_name: use_tls
     type: boolean
     details: Using TLS for connection
+  - field_name: provider_verify_certificate
+    type: boolean
+    details: Whether PostgreSQL Terraform provider should validate the server certificate. The AWS certificate bundle must be installed.
 bind:
   plan_inputs: []
   user_inputs: []
@@ -345,6 +352,10 @@ bind:
     type: boolean
     default: ${instance.details["use_tls"]}
     overwrite: true    
+  - name: provider_verify_certificate
+    type: boolean
+    default: ${instance.details["provider_verify_certificate"]}
+    overwrite: true
   template_refs:
     outputs: ./terraform/postgresql/bind/outputs.tf
     provider: ./terraform/postgresql/bind/provider.tf
@@ -368,35 +379,35 @@ examples:
 - name: small
   description: Create a small PostgreSQL instance
   plan_id: ffc51616-228b-41bd-bed1-d601c18d58f5
-  provision_params: { "publicly_accessible": true, "use_tls": false }
+  provision_params: { "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: medium
   description: Create a medium PostgreSQL instance
   plan_id: e64d07f9-ceb2-40a6-abd9-391047fa3cf5
-  provision_params: { "publicly_accessible": true, "use_tls": false }
+  provision_params: { "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: large
   description: Create a large PostgreSQL instance
   plan_id: 48baef10-a14c-4ae1-aab5-25f26eba941a
-  provision_params: { "publicly_accessible": true, "use_tls": false }
+  provision_params: { "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: medium-multiaz
   description: Create a medium multiaz PostgreSQL instance
   plan_id: e64d07f9-ceb2-40a6-abd9-391047fa3cf5
-  provision_params: { "publicly_accessible": true, "multi_az": true, "use_tls": false }
+  provision_params: { "publicly_accessible": true, "multi_az": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: small-encrypted
   description: Create a small PostgreSQL instance with encrypted storage
   plan_id: ffc51616-228b-41bd-bed1-d601c18d58f5
-  provision_params: { "storage_encrypted": true, "publicly_accessible": true, "use_tls": false }
+  provision_params: { "storage_encrypted": true, "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: small-disable-upgrades
   description: Create a small PostgreSQL instance with disabled upgrades
   plan_id: ffc51616-228b-41bd-bed1-d601c18d58f5
-  provision_params: { "allow_major_version_upgrade": false, "auto_minor_version_upgrade": false, "publicly_accessible": true, "use_tls": false }
+  provision_params: { "allow_major_version_upgrade": false, "auto_minor_version_upgrade": false, "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}
 - name: small-with-maintenance-window
   description: Create a small PostgreSQL instance with encrypted storage
   plan_id: ffc51616-228b-41bd-bed1-d601c18d58f5
-  provision_params: { "maintenance_day": "Thu", "maintenance_start_hour": "15", "maintenance_start_min": "30", "maintenance_end_hour": "17", "maintenance_end_min": "00", "publicly_accessible": true, "use_tls": false }
+  provision_params: { "maintenance_day": "Thu", "maintenance_start_hour": "15", "maintenance_start_min": "30", "maintenance_end_hour": "17", "maintenance_end_min": "00", "publicly_accessible": true, "provider_verify_certificate": false }
   bind_params: {}

terraform/postgresql/bind/provider.tf
@@ -19,5 +19,5 @@ provider "postgresql" {
   password  = var.admin_password
   superuser = false
   database  = var.db_name
-  sslmode   = var.use_tls ? "verify-full" : "prefer"
+  sslmode   = var.provider_verify_certificate ? "verify-full" : "require"
 }

terraform/postgresql/bind/variables.tf
@@ -18,3 +18,4 @@ variable "port" { type = number }
 variable "admin_username" { type = string }
 variable "admin_password" { type = string }
 variable "use_tls" { type = bool }
+variable "provider_verify_certificate" { type = bool }

terraform/postgresql/provision/outputs.tf
@@ -18,6 +18,7 @@ output "port" { value = local.ports[var.engine] }
 output "username" { value = aws_db_instance.db_instance.username }
 output "password" { value = aws_db_instance.db_instance.password }
 output "use_tls" { value = var.use_tls }
+output "provider_verify_certificate" { value = var.provider_verify_certificate }
 output "status" { value = format("created db %s (id: %s) on server %s URL: https://%s.console.aws.amazon.com/rds/home?region=%s#database:id=%s;is-cluster=false",
   aws_db_instance.db_instance.name,
   aws_db_instance.db_instance.id,

terraform/postgresql/provision/variables.tf
@@ -34,3 +34,4 @@ variable "allow_major_version_upgrade" { type = bool }
 variable "auto_minor_version_upgrade" { type = bool }
 variable "maintenance_window" { type = string }
 variable "use_tls" { type = bool }
+variable "provider_verify_certificate" { type = bool }