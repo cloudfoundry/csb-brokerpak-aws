a32cd1174e44073e865f66d550e04a68327df04f
George Blue <gblue@pivotal.io>
2022-08-02 21:53:37 +0100

    Revert "feat: new Terraform provider for PostgreSQL (#391)"
    This reverts commit a6710ec10fc9b9309b0120148df89d006d261bca.

--- terraform/postgresql/bind/main.tf
+++ terraform/postgresql/bind/main.tf
@@ -12,21 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-resource "random_string" "username" {
-  length  = 16
-  special = false
-  number  = false
+resource "postgresql_role" "new_user" {
+  name                = random_string.username.result
+  login               = true
+  password            = random_password.password.result
+  skip_reassign_owned = true
+  skip_drop_role      = true
 }
 
-resource "random_password" "password" {
-  length           = 64
-  override_special = "~_-."
-  min_upper        = 2
-  min_lower        = 2
-  min_special      = 2
+resource "postgresql_grant" "all_access" {
+  depends_on = [postgresql_role.new_user]
+  database   = var.db_name
+  role       = postgresql_role.new_user.name
+  #   schema      = "public"
+  object_type = "database"
+  privileges  = ["ALL"]
 }
 
-resource "csbpg_binding_user" "binding_user" {
-  username = random_string.username.result
-  password = random_password.password.result
+resource "postgresql_grant" "table_access" {
+  depends_on  = [postgresql_role.new_user]
+  database    = var.db_name
+  role        = postgresql_role.new_user.name
+  schema      = "public"
+  object_type = "table"
+  privileges  = ["ALL"]
 }
