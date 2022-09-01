3053148eb28d1d1c0bad320214cf83e0a3a8c4e1
George Blue <gblue@pivotal.io>
2022-09-01 14:22:05 +0100

    feat: Aurora PostgreSQL foundations
    This introduces the foundations of a new Aurora PostgreSQL service that works end to end,
    and can have features added to it.
    
    [#183001781](https://www.pivotaltracker.com/story/show/183001781)

---
+++ terraform/aurora-postgresql/provision/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_access_key_id" { type = string }
+variable "aws_secret_access_key" { type = string }
+variable "region" { type = string }
+variable "instance_name" { type = string }
+variable "aws_vpc_id" { type = string }
