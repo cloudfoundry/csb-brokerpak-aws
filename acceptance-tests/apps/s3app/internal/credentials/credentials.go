b5c5cf6200f893341929831037cafe903beada7e
Felisia Martini <fmartini@pivotal.io>
2022-09-07 11:11:50 +0100

    test: Add test for s3 migration from legacy (#497)
    Test validates that the `aws s3 sync` command can be used to migrate S3 instances from the old broker to the new broker.
    
    [#182845406](https://www.pivotaltracker.com/story/show/182845406)

--- acceptance-tests/apps/s3app/internal/credentials/credentials.go
+++ acceptance-tests/apps/s3app/internal/credentials/credentials.go
@@ -16,19 +16,59 @@ type S3Service struct {
 	Arn              string `mapstructure:"arn"`
 }
 
+type S3ServiceLegacy struct {
+	AccessKeyId     string `mapstructure:"access_key_id"`
+	AccessKeySecret string `mapstructure:"secret_access_key"`
+	Region          string `mapstructure:"region"`
+	BucketName      string `mapstructure:"bucket"`
+}
+
 func Read() (S3Service, error) {
+	serviceTag, serviceCred, err := findService()
+	if err != nil {
+		return S3Service{}, err
+	}
+	switch serviceTag {
+	case "s3":
+		return ReadCSBS3(serviceCred)
+	case "aws-s3":
+		return ReadLegacyS3(serviceCred)
+	}
+
+	return S3Service{}, fmt.Errorf("unable to find credentials for S3")
+}
+
+func findService() (string, cfenv.Service, error) {
 	app, err := cfenv.Current()
 	if err != nil {
-		return S3Service{}, fmt.Errorf("error reading app env: %w", err)
-	}
-	svs, err := app.Services.WithTag("s3")
-	if err != nil {
-		return S3Service{}, fmt.Errorf("error reading S3 service details")
+		return "", cfenv.Service{}, fmt.Errorf("error reading app env: %w", err)
 	}
 
+	for _, f := range []func() (string, []cfenv.Service, error){
+		func() (string, []cfenv.Service, error) {
+			serviceTag := "s3"
+			srv, err := app.Services.WithTag(serviceTag)
+			return serviceTag, srv, err
+
+		},
+		func() (string, []cfenv.Service, error) {
+			serviceLabel := "aws-s3"
+			srv, err := app.Services.WithLabel(serviceLabel)
+			return serviceLabel, srv, err
+		},
+	} {
+		serviceType, svs, err := f()
+		if err == nil && len(svs) > 0 {
+			return serviceType, svs[0], nil
+		}
+	}
+
+	return "", cfenv.Service{}, fmt.Errorf("unable to find credentials for S3")
+}
+
+func ReadCSBS3(svs cfenv.Service) (S3Service, error) {
 	var s S3Service
-
-	if err := mapstructure.Decode(svs[0].Credentials, &s); err != nil {
+	if err := mapstructure.Decode(svs.Credentials, &s); err != nil {
 		return S3Service{}, fmt.Errorf("failed to decode credentials: %w", err)
 	}
 
@@ -42,4 +82,25 @@ func Read() (S3Service, error) {
 	}
 
 	return s, nil
+}
+
+func ReadLegacyS3(svs cfenv.Service) (S3Service, error) {
+	var s S3ServiceLegacy
+	if err := mapstructure.Decode(svs.Credentials, &s); err != nil {
+		return S3Service{}, fmt.Errorf("failed to decode credentials: %w", err)
+	}
+
+	if s.AccessKeyId == "" ||
+		s.AccessKeySecret == "" ||
+		s.Region == "" ||
+		s.BucketName == "" {
+		return S3Service{}, fmt.Errorf("parsed credentials are not valid")
+	}
+
+	return S3Service{
+		AccessKeyId:     s.AccessKeyId,
+		AccessKeySecret: s.AccessKeySecret,
+		Region:          s.Region,
+		BucketName:      s.BucketName,
+	}, nil
 }
