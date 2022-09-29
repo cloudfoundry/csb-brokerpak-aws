4b45ac9325c4f461adee8172ddaa4d5b790e1a6a
George Blue <blgm@users.noreply.github.com>
2022-09-29 17:22:29 +0100

    feat(aurora): ability to configure number of instances (#524)
    [#183389893](https://www.pivotaltracker.com/story/show/183389893)

--- terraform/aurora-postgresql/provision/variables.tf
+++ terraform/aurora-postgresql/provision/variables.tf
@@ -3,3 +3,4 @@ variable "aws_secret_access_key" { type = string }
 variable "region" { type = string }
 variable "instance_name" { type = string }
 variable "aws_vpc_id" { type = string }
+variable "cluster_instances" { type = number }
