698790592198eda7427d4c581c3c7640a6d4fe65
Andrea Zucchini <zandrea@vmware.com>
2022-11-02 17:31:29 +0000

    chore: old terraform provider is no longer needed
    [#183650281](https://www.pivotaltracker.com/story/show/183650281)

--- terraform/aurora-mysql/bind/provider.tf
+++ terraform/aurora-mysql/bind/provider.tf
@@ -1,10 +1,3 @@
-provider "mysql" {
-  endpoint = format("%s:%d", var.hostname, var.port)
-  username = var.admin_username
-  password = var.admin_password
-  tls      = true
-}
-
 provider "csbmysql" {
   database = var.name
   password = var.admin_password
