0523ac8babea2b3546b8cd801e845cfd4fbab99c
Andrea Zucchini <zandrea@vmware.com>
2022-11-02 16:15:19 +0000

    feat: TLS connections always enabled
    Using the new TF provider, by default, all users will connect using TLS
    due to the configuration per-account level.
    
    [#183650281](https://www.pivotaltracker.com/story/show/183650281)

--- terraform/aurora-mysql/bind/provider.tf
+++ terraform/aurora-mysql/bind/provider.tf
@@ -2,5 +2,13 @@ provider "mysql" {
   endpoint = format("%s:%d", var.hostname, var.port)
   username = var.admin_username
   password = var.admin_password
-  tls      = false
+  tls      = true
+}
+
+provider "csbmysql" {
+  database = var.name
+  password = var.admin_password
+  username = var.admin_username
+  port     = var.port
+  host     = var.hostname
 }
