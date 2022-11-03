a89a5b633557fcd7083ec1287b4fab03313ee020
Andrea Zucchini <zandrea@vmware.com>
2022-11-03 09:36:23 +0000

    chore: subnet group modification force cluster recreation (#606)
    [#183702197](https://www.pivotaltracker.com/story/show/183702197)

--- integration-tests/aurora_postgresql_test.go
+++ integration-tests/aurora_postgresql_test.go
@@ -163,6 +163,7 @@ var _ = Describe("Aurora PostgreSQL", Label("aurora-postgresql"), func() {
 			Entry("region", "region", "no-matter-what-region"),
 			Entry("instance_name", "instance_name", "marmaduke"),
 			Entry("db_name", "db_name", "someNewName"),
+			Entry("rds_subnet_group", "rds_subnet_group", "some-new-subnet-name"),
 		)
 
 		DescribeTable(
@@ -177,7 +178,6 @@ var _ = Describe("Aurora PostgreSQL", Label("aurora-postgresql"), func() {
 			Entry("allow_major_version_upgrade", "allow_major_version_upgrade", false),
 			Entry("auto_minor_version_upgrade", "auto_minor_version_upgrade", false),
 			Entry("rds_vpc_security_group_ids", "rds_vpc_security_group_ids", "group3"),
-			Entry("rds_subnet_group", "rds_subnet_group", "other-sn"),
 		)
 	})
 })
