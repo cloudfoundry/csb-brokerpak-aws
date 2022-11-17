1db62499251f9f8d4ffade57db15b8bb12801f74
Andrea Zucchini <zandrea@vmware.com>
2022-11-17 12:06:04 +0000

    feat: Expose preferred_maintenance_window property - Aurora MySQL (#646)
    [#183535519](https://www.pivotaltracker.com/story/show/183535519)

--- terraform/aurora-mysql/provision/data.tf
+++ terraform/aurora-mysql/provision/data.tf
@@ -18,4 +18,21 @@ locals {
   subnet_group               = length(var.rds_subnet_group) > 0 ? var.rds_subnet_group : aws_db_subnet_group.rds_private_subnet[0].name
 
   log_groups = var.enable_audit_logging == true ? { "audit" : true } : {}
+
+  is_maintenance_window_blank = length(compact([
+    var.preferred_maintenance_day,
+    var.preferred_maintenance_start_hour,
+    var.preferred_maintenance_end_hour,
+    var.preferred_maintenance_start_min,
+    var.preferred_maintenance_end_min
+  ])) == 0
+
+  preferred_maintenance_window = local.is_maintenance_window_blank ? null : format("%s:%s:%s-%s:%s:%s",
+    var.preferred_maintenance_day,
+    var.preferred_maintenance_start_hour,
+    var.preferred_maintenance_start_min,
+    var.preferred_maintenance_day,
+    var.preferred_maintenance_end_hour,
+    var.preferred_maintenance_end_min
+  )
 }
