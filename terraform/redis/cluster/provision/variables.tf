34872757566dbdad1003bec12fda4ba4cf4765c9
George Blue <blgm@users.noreply.github.com>
2023-03-23 16:47:46 +0000

    feat(redis): new preferred_azs property (#829)
    [#184614709](https://www.pivotaltracker.com/story/show/184614709)

--- terraform/redis/cluster/provision/variables.tf
+++ terraform/redis/cluster/provision/variables.tf
@@ -39,3 +39,4 @@ variable "backup_start_min" { type = string }
 variable "backup_end_hour" { type = string }
 variable "backup_end_min" { type = string }
 variable "parameter_group_name" { type = string }
+variable "preferred_azs" { type = list(string) }
