9e67a9b1fe91a4c545ce62e0b8735701a13d79a4
Andrea Zucchini <zandrea@vmware.com>
2023-04-14 11:15:03 +0100

    test: [aurora-mysql] add upgrade tests (#889)
    * fix: apply immediately to avoid inconsistencies in the provider
    
    [#184888948](https://www.pivotaltracker.com/story/show/184888948)
    
    * fix: upgrade major engine version with default parameter group
    
    [#184919093](https://www.pivotaltracker.com/story/show/184919093)
    
    * chore: be more explicit in the used plans
    
    [#184919093](https://www.pivotaltracker.com/story/show/184919093)
    
    * chore: clean file
    
    [#184919093](https://www.pivotaltracker.com/story/show/184919093)
    
    * chore: remove unnecessary step
    
    [#184919093](https://www.pivotaltracker.com/story/show/184919093)
    
    * test: [aurora-mysql] add upgrade tests
    
    [#184919093](https://www.pivotaltracker.com/story/show/184919093)

---
+++ acceptance-tests/upgrade/update_and_upgrade_aurora_mysql_test.go
@@ -0,0 +1,91 @@
+package upgrade_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"csbbrokerpakaws/acceptance-tests/helpers/apps"
+	"csbbrokerpakaws/acceptance-tests/helpers/brokers"
+	"csbbrokerpakaws/acceptance-tests/helpers/random"
+	"csbbrokerpakaws/acceptance-tests/helpers/services"
+)
+
+var _ = Describe("UpgradeAuroraMySQLTest", Label("aurora-mysql", "upgrade"), func() {
+	When("upgrading broker version", func() {
+		It("should continue to work", func() {
+			By("pushing latest released broker version")
+			serviceBroker := brokers.Create(
+				brokers.WithPrefix("csb-aurora-mysql"),
+				brokers.WithSourceDir(releasedBuildDir),
+				brokers.WithReleaseEnv(releasedBuildDir),
+			)
+			defer serviceBroker.Delete()
+
+			By("creating a service")
+			serviceInstance := services.CreateInstance(
+				"csb-aws-aurora-mysql",
+				services.WithPlan("default"),
+				services.WithParameters(
+					map[string]any{
+						"cluster_instances": 1,
+						"instance_class":    "db.r5.large",
+						"engine_version":    "8.0.mysql_aurora.3.02.2",
+					}),
+				services.WithBroker(serviceBroker),
+			)
+			defer serviceInstance.Delete()
+
+			By("pushing the unstarted app twice")
+			appOne := apps.Push(apps.WithApp(apps.MySQL))
+			appTwo := apps.Push(apps.WithApp(apps.MySQL))
+			defer apps.Delete(appOne, appTwo)
+
+			By("binding to the apps")
+			bindingOne := serviceInstance.Bind(appOne)
+			bindingTwo := serviceInstance.Bind(appTwo)
+
+			By("starting the apps")
+			apps.Start(appOne, appTwo)
+
+			By("setting a key-value using the first app")
+			key := random.Hexadecimal()
+			value := random.Hexadecimal()
+			appOne.PUT(value, key)
+			By("getting the value using the second app")
+			Expect(appTwo.GET(key).String()).To(Equal(value))
+
+			By("pushing the development version of the broker")
+			serviceBroker.UpdateBroker(developmentBuildDir)
+
+			By("upgrading service instance")
+			serviceInstance.Upgrade()
+
+			By("getting the value using the second app")
+			Expect(appTwo.GET(key).String()).To(Equal(value))
+
+			By("updating the instance")
+			serviceInstance.Update(services.WithParameters(map[string]any{"auto_minor_version_upgrade": false}))
+
+			By("getting the value using the second app")
+			Expect(appTwo.GET(key).String()).To(Equal(value))
+
+			By("deleting bindings created before the upgrade")
+			bindingOne.Unbind()
+			bindingTwo.Unbind()
+
+			By("creating new bindings and testing they still work")
+			serviceInstance.Bind(appOne)
+			serviceInstance.Bind(appTwo)
+			apps.Restage(appOne, appTwo)
+
+			By("getting the value using the second app")
+			Expect(appTwo.GET(key).String()).To(Equal(value))
+
+			By("checking data can still be written and read")
+			keyTwo := random.Hexadecimal()
+			valueTwo := random.Hexadecimal()
+			appOne.PUT(valueTwo, keyTwo)
+			Expect(appTwo.GET(keyTwo).String()).To(Equal(valueTwo))
+		})
+	})
+})
