b273738005b9452a0f29f3771d260649b2cb877f
Andrea Zucchini <zandrea@vmware.com>
2023-05-10 12:45:11 +0100

    feat: [MSSQL] create service foundations
    [#185145337](https://www.pivotaltracker.com/story/show/185145337)

---
+++ terraform/mssql/bind/variables.tf
@@ -0,0 +1,8 @@
+variable "db_name" { type = string }
+variable "hostname" { type = string }
+variable "admin_username" { type = string }
+variable "admin_password" { type = string }
+
+locals {
+  port = 3306
+}
