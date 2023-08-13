222efee3ee3963432f1861492b196025c44e2053
Andrea Zucchini <zandrea@vmware.com>
2023-08-13 12:49:30 +0100

    chore: remove test data
    [#185151996](https://www.pivotaltracker.com/story/show/185151996)

--- terraform/mssql/bind/variables.tf
+++ terraform/mssql/bind/variables.tf
@@ -1,18 +1,14 @@
 variable "db_name" {
   type = string
-  default = "my-db"
 }
 variable "hostname" {
   type = string
-  default = "database-azucgar.crvbjnvu3aun.us-west-2.rds.amazonaws.com"
 }
 variable "admin_username" {
   type = string
-  default = "admin"
 }
 variable "admin_password" {
   type = string
-  default = "12345678"
 }
 
 locals {
