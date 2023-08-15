9293a7b5fd071cd01342b07aa60caa1327f9ba8a
Andrea Zucchini <zandrea@vmware.com>
2023-08-15 17:22:17 +0100

    feat: integration mssql Terraform provider
    [#185152034](https://www.pivotaltracker.com/story/show/185152034)

---
+++ acceptance-tests/apps/mssqlapp/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"mssqlapp/internal/app"
+	"mssqlapp/internal/credentials"
+)
+
+func main() {
+	log.Println("Starting.")
+
+	log.Println("Reading credentials.")
+	config, err := credentials.Read()
+	if err != nil {
+		log.Fatalf("failed to read credentials: %s", err)
+	}
+
+	port := port()
+	log.Printf("Listening on port: %s", port)
+	http.Handle("/", app.App(config))
+	http.ListenAndServe(port, nil)
+}
+
+func port() string {
+	if port := os.Getenv("PORT"); port != "" {
+		return fmt.Sprintf(":%s", port)
+	}
+	return ":8080"
+}
