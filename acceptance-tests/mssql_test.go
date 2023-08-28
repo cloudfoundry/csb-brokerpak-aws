1737887f5bb8405ab5605c44734948712e42230a
Andrea Zucchini <zandrea@vmware.com>
2023-08-28 13:53:17 +0100

    test: drop schema using the correct binding
    [#185152034](https://www.pivotaltracker.com/story/show/185152034)

--- acceptance-tests/mssql_test.go
+++ acceptance-tests/mssql_test.go
@@ -46,8 +46,13 @@ var _ = Describe("MSSQL", Label("mssql"), func() {
 		got := appTwo.GET("%s/%s", schema, key).String()
 		Expect(got).To(Equal(value))
 
-		By("dropping the schema using the first app")
-		appOne.DELETE(schema)
+		By("deleting binding one the binding two keeps reading the value")
+		binding.Unbind()
+		got = appTwo.GET("%s/%s", schema, key).String()
+		Expect(got).To(Equal(value))
+
+		By("dropping the schema using the second app")
+		appTwo.DELETE(schema)
 	})
 
 	It("can't be destroyed if `deletion_protection: true`", func() {
