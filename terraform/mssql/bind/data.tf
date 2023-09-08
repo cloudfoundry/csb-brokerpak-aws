2cf3b283c18eb66a35917fc3ebe4c9afa0069396
Andrea Zucchini <zandrea@vmware.com>
2023-09-08 13:37:10 +0100

    chore: format code
    [#185844429](https://www.pivotaltracker.com/story/show/185844429)

--- terraform/mssql/bind/data.tf
+++ terraform/mssql/bind/data.tf
@@ -1,4 +1,4 @@
 locals {
   jdbc_tls_string = (var.require_ssl ? format("encrypt=true;trustServerCertificate=false;hostNameInCertificate=%s", var.hostname) : "encrypt=disabled")
-  uri_tls_string = (var.require_ssl ? format("encrypt=true&TrustServerCertificate=false&HostNameInCertificate=%s", var.hostname) : "encrypt=disabled")
+  uri_tls_string  = (var.require_ssl ? format("encrypt=true&TrustServerCertificate=false&HostNameInCertificate=%s", var.hostname) : "encrypt=disabled")
 }
