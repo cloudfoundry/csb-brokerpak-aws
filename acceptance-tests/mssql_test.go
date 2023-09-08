7276daab8675e366a436f4860e5d25ade237e4ea
Andrea Zucchini <zandrea@vmware.com>
2023-09-08 13:15:47 +0100

    test: acceptance test
    Both GoLang and Java applications work under SSL.
    
    [#185844429](https://www.pivotaltracker.com/story/show/185844429)

--- acceptance-tests/mssql_test.go
+++ acceptance-tests/mssql_test.go
@@ -1,7 +1,11 @@
 package acceptance_tests_test
 
 import (
+	"io"
+	"net/http"
+
 	"csbbrokerpakaws/acceptance-tests/helpers/apps"
+	"csbbrokerpakaws/acceptance-tests/helpers/jdbcapp"
 	"csbbrokerpakaws/acceptance-tests/helpers/matchers"
 	"csbbrokerpakaws/acceptance-tests/helpers/random"
 	"csbbrokerpakaws/acceptance-tests/helpers/services"
@@ -18,6 +22,7 @@ var _ = Describe("MSSQL", Label("mssql"), func() {
 			// Every time we test the service using the cf CLI we do it with backups enabled as it is the
 			// default option. The idea is to speed up the execution of the test.
 			"backup_retention_period": 0,
+			"require_ssl":             true,
 		}
 
 		serviceInstance := services.CreateInstance(
@@ -55,6 +60,14 @@ var _ = Describe("MSSQL", Label("mssql"), func() {
 		got := appTwo.GET("%s/%s", schema, key).String()
 		Expect(got).To(Equal(value))
 
+		By("verifying that non-TLS connections should fail")
+		response := appTwo.GETResponse("%s/%s?tls=disable", schema, key)
+		defer response.Body.Close()
+		Expect(response).To(HaveHTTPStatus(http.StatusInternalServerError), "force TLS is enabled by default")
+		b, err := io.ReadAll(response.Body)
+		Expect(err).ToNot(HaveOccurred(), "error reading response body in TLS failure")
+		Expect(string(b)).To(ContainSubstring("TLS Handshake failed: cannot read handshake packet: EOF"), "force TLS is enabled by default")
+
 		By("deleting binding one the binding two keeps reading the value")
 		binding.Unbind()
 		got = appTwo.GET("%s/%s", schema, key).String()
@@ -62,6 +75,61 @@ var _ = Describe("MSSQL", Label("mssql"), func() {
 
 		By("dropping the schema using the second app")
 		appTwo.DELETE(schema)
+	})
+
+	It("can be accessed by a JAVA app using the JDBC URL", Label("JDBC-mssql"), func() {
+		var (
+			userIn  jdbcapp.AppResponseUser
+			userOut jdbcapp.AppResponseUser
+		)
+
+		By("creating a service instance")
+		params := map[string]any{
+			// The backups add quite some time to acceptance test.
+			// Every time we test the service using the cf CLI we do it with backups enabled as it is the
+			// default option. The idea is to speed up the execution of the test.
+			"backup_retention_period": 0,
+			"require_ssl":             true,
+		}
+
+		serviceInstance := services.CreateInstance(
+			"csb-aws-mssql",
+			services.WithPlan("default"),
+			services.WithParameters(params),
+		)
+		defer serviceInstance.Delete()
+
+		By("pushing the unstarted app twice")
+		manifest := jdbcapp.ManifestFor(jdbcapp.SQLServer)
+		appWriter := apps.Push(apps.WithApp(apps.JDBCTestAppSQLServer), apps.WithManifest(manifest))
+		appReader := apps.Push(apps.WithApp(apps.JDBCTestAppSQLServer), apps.WithManifest(manifest))
+		defer apps.Delete(appWriter, appReader)
+
+		By("binding the the writer app")
+		serviceInstance.Bind(appWriter)
+
+		By("starting the writer app")
+		apps.Start(appWriter)
+
+		By("creating an entry using the writer app")
+		value := random.Hexadecimal()
+		appWriter.POST("", "?name=%s", value).ParseInto(&userIn)
+
+		By("binding the reader app")
+		serviceInstance.Bind(appReader)
+
+		By("starting the reader app")
+		apps.Start(appReader)
+
+		By("getting the entry using the reader app")
+		appReader.GET("%d", userIn.ID).ParseInto(&userOut)
+		Expect(userOut.Name).To(Equal(value), "The first app stored [%s] as the value, the second app retrieved [%s]", value, userOut.Name)
+
+		// This step is not necessary, added for the purpose of serving as documentation
+		By("verifying the DB connection utilises TLS")
+		httpResponse := appWriter.GETResponse("sqlserver-ssl")
+		defer httpResponse.Body.Close()
+		Expect(httpResponse.StatusCode).To(BeNumerically("==", http.StatusInternalServerError), "it can be run only by administrators with the VIEW SERVER STATE privilege")
 	})
 
 	It("can't be destroyed if `deletion_protection: true`", func() {
