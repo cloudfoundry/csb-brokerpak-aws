6023d852ec3b2f7dff69dd4396b38301e2e82f02
Andrea Zucchini <zandrea@vmware.com>
2023-09-08 17:08:54 +0100

    test: simplify test
    [#185844429](https://www.pivotaltracker.com/story/show/185844429)

--- acceptance-tests/mssql_test.go
+++ acceptance-tests/mssql_test.go
@@ -6,7 +6,6 @@ import (
 
 	"csbbrokerpakaws/acceptance-tests/helpers/apps"
 	"csbbrokerpakaws/acceptance-tests/helpers/jdbcapp"
-	"csbbrokerpakaws/acceptance-tests/helpers/matchers"
 	"csbbrokerpakaws/acceptance-tests/helpers/random"
 	"csbbrokerpakaws/acceptance-tests/helpers/services"
 
@@ -15,68 +14,6 @@ import (
 )
 
 var _ = Describe("MSSQL", Label("mssql"), func() {
-	It("can be created", func() {
-		By("creating a service instance")
-		params := map[string]any{
-			// The backups add quite some time to acceptance test.
-			// Every time we test the service using the cf CLI we do it with backups enabled as it is the
-			// default option. The idea is to speed up the execution of the test.
-			"backup_retention_period": 0,
-			"require_ssl":             true,
-		}
-
-		serviceInstance := services.CreateInstance(
-			"csb-aws-mssql",
-			services.WithPlan("default"),
-			services.WithParameters(params),
-		)
-		defer serviceInstance.Delete()
-
-		By("pushing the unstarted app twice")
-		appOne := apps.Push(apps.WithApp(apps.MSSQL))
-		appTwo := apps.Push(apps.WithApp(apps.MSSQL))
-		defer apps.Delete(appOne, appTwo)
-
-		By("binding the apps to the service instance")
-		binding := serviceInstance.Bind(appOne)
-		serviceInstance.Bind(appTwo)
-
-		By("starting the apps")
-		apps.Start(appOne, appTwo)
-
-		By("checking that the app environment has a credhub reference for credentials")
-		Expect(binding.Credential()).To(matchers.HaveCredHubRef)
-
-		By("creating a schema using the first app")
-		schema := random.Name(random.WithMaxLength(10))
-		appOne.PUT("", schema)
-
-		By("setting a key-value using the first app")
-		key := random.Hexadecimal()
-		value := random.Hexadecimal()
-		appOne.PUT(value, "%s/%s", schema, key)
-
-		By("getting the value using the second app")
-		got := appTwo.GET("%s/%s", schema, key).String()
-		Expect(got).To(Equal(value))
-
-		By("verifying that non-TLS connections should fail")
-		response := appTwo.GETResponse("%s/%s?tls=disable", schema, key)
-		defer response.Body.Close()
-		Expect(response).To(HaveHTTPStatus(http.StatusInternalServerError), "force TLS is enabled by default")
-		b, err := io.ReadAll(response.Body)
-		Expect(err).ToNot(HaveOccurred(), "error reading response body in TLS failure")
-		Expect(string(b)).To(ContainSubstring("TLS Handshake failed: cannot read handshake packet: EOF"), "force TLS is enabled by default")
-
-		By("deleting binding one the binding two keeps reading the value")
-		binding.Unbind()
-		got = appTwo.GET("%s/%s", schema, key).String()
-		Expect(got).To(Equal(value))
-
-		By("dropping the schema using the second app")
-		appTwo.DELETE(schema)
-	})
-
 	It("can be accessed by a JAVA app using the JDBC URL", Label("JDBC-mssql"), func() {
 		var (
 			userIn  jdbcapp.AppResponseUser
@@ -130,6 +67,42 @@ var _ = Describe("MSSQL", Label("mssql"), func() {
 		httpResponse := appWriter.GETResponse("sqlserver-ssl")
 		defer httpResponse.Body.Close()
 		Expect(httpResponse.StatusCode).To(BeNumerically("==", http.StatusInternalServerError), "it can be run only by administrators with the VIEW SERVER STATE privilege")
+
+		By("pushing and binding two apps for verifying non-TLS connection attempts and object reassignment")
+		golangAppOne := apps.Push(apps.WithApp(apps.MSSQL))
+		golangAppTwo := apps.Push(apps.WithApp(apps.MSSQL))
+		defer apps.Delete(golangAppOne, golangAppTwo)
+
+		By("binding the apps to the service instance")
+		binding := serviceInstance.Bind(golangAppOne)
+		serviceInstance.Bind(golangAppTwo)
+
+		By("starting the apps")
+		apps.Start(golangAppOne, golangAppTwo)
+		By("creating a schema using the first app")
+		schema := random.Name(random.WithMaxLength(10))
+		golangAppOne.PUT("", schema)
+
+		By("setting a key-value using the first app")
+		key := random.Hexadecimal()
+		value = random.Hexadecimal()
+		golangAppOne.PUT(value, "%s/%s", schema, key)
+
+		By("verifying that non-TLS connections should fail")
+		response := golangAppTwo.GETResponse("%s/%s?tls=disable", schema, key)
+		defer response.Body.Close()
+		Expect(response).To(HaveHTTPStatus(http.StatusInternalServerError), "force TLS is enabled by default")
+		b, err := io.ReadAll(response.Body)
+		Expect(err).ToNot(HaveOccurred(), "error reading response body in TLS failure")
+		Expect(string(b)).To(ContainSubstring("TLS Handshake failed: cannot read handshake packet: EOF"), "force TLS is enabled by default")
+
+		By("deleting binding one the binding two keeps reading the value - object reassignment works")
+		binding.Unbind()
+		got := golangAppTwo.GET("%s/%s", schema, key).String()
+		Expect(got).To(Equal(value))
+
+		By("dropping the schema using the second app")
+		golangAppTwo.DELETE(schema)
 	})
 
 	It("can't be destroyed if `deletion_protection: true`", func() {
