bb1f3be4e6b9d8e6e3b22af527f3ad660ae8d20a
Fernando Naranjo <fnaranjo@vmware.com>
2023-11-09 19:57:18 +0100

    chore: allow running terraform without passing any vars
    By itself, this change is pretty useless since many conditions,
    properties, etc. will error preventing doing anything useful.
    
    However, when complemeted with another strategy (presented in
    subsequent commits) it will allow reusing the same inputs
    as the last execution.

--- terraform/s3/provision/provider.tf
+++ terraform/s3/provision/provider.tf
@@ -14,7 +14,10 @@
 
 variable "aws_access_key_id" { type = string }
 variable "aws_secret_access_key" { type = string }
-variable "region" { type = string }
+variable "region" {
+  type    = string
+  default = null
+}
 
 provider "aws" {
   region     = var.region
