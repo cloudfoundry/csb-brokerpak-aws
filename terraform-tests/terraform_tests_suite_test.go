b91ba4dabcc5ccbeec23dfaaa3e80546902a8919
fnaranjo <fnaranjo@vmware.com>
2023-11-14 18:44:17 +0100

    chore: allow passing different VPC to fix tests in GovCloud
    [#186311950](https://www.pivotaltracker.com/story/show/186311950)

--- terraform-tests/terraform_tests_suite_test.go
+++ terraform-tests/terraform_tests_suite_test.go
@@ -22,7 +22,7 @@ var (
 
 	awsSecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
 	awsAccessKeyID     = os.Getenv("AWS_ACCESS_KEY_ID")
-	awsVPCID           = "vpc-72464617"
+	awsVPCID           = getAWSVPCID()
 	awsRegion          = getAWSRegion()
 )
 
@@ -55,4 +55,13 @@ func getAWSRegionFromCSBDefaults() string {
 		return matches[1]
 	}
 	return ""
+}
+
+func getAWSVPCID() string {
+	envVPCID, defined := os.LookupEnv("AWS_PAS_VPC_ID")
+	if defined {
+		return envVPCID
+	}
+
+	return "vpc-72464617"
 }
