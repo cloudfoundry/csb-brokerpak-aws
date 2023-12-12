14e5302ca9a5cb7842ee011d8f79e470e9b19c48
George Blue <blgm@users.noreply.github.com>
2023-12-12 17:15:49 +0000

    fix: remove obsolete mysql and postgres providers from versions file (#1404)
    [#186588913](https://www.pivotaltracker.com/story/show/186588913)

--- terraform/postgresql/bind/versions.tf
+++ terraform/postgresql/bind/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = ">= 1.16.0"
-    }
     csbpg = {
       source  = "cloud-service-broker/csbpg"
       version = ">= 1.0.1"
