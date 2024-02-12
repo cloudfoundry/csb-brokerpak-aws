3d55fe618afa5e38c341f4ba8c4cd1632b13bf20
George Blue <gblue@pivotal.io>
2024-02-12 12:29:54 +0000

    test: basic test app for SQS
    This is a basic Go test app for SQS. We may want to replace it with a
    Java app in time.
    
    [#186787985](https://www.pivotaltracker.com/story/show/186787985)

---
+++ acceptance-tests/apps/sqsapp/internal/app/send.go
@@ -0,0 +1,48 @@
+package app
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sqsapp/internal/credentials"
+
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+func handleSend(creds credentials.Credentials) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Handling send.")
+
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			fail(w, http.StatusBadRequest, "could not read body: %q", err)
+			return
+		}
+		defer r.Body.Close()
+		body := string(data)
+
+		cfg, err := creds.Config()
+		if err != nil {
+			fail(w, http.StatusInternalServerError, "could not read AWS config: %q", err)
+			return
+		}
+
+		output, err := sqs.NewFromConfig(cfg).SendMessage(r.Context(), &sqs.SendMessageInput{
+			MessageBody: &body,
+			QueueUrl:    &creds.URL,
+		})
+		if err != nil {
+			fail(w, http.StatusBadRequest, "error sending message: %q", err)
+			return
+		}
+
+		id := *output.MessageId
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(fmt.Sprintf(`{"id":"%s"}`, id)))
+
+		log.Printf("sent message ID: %q\n", id)
+	}
+}
