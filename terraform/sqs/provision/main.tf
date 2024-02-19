09669bbdcc1a4b036a44969bb04d640276bcdc10
George Blue <blgm@users.noreply.github.com>
2024-02-19 13:50:38 +0000

    chore: use default tags for SQS (#1529)
    By using default tags configured on the provider, we avoid having to add
    them to every resource. At this stage it doesn't make a lot of
    difference, but if we were to add more resources then it would reduce
    duplication.
    
    Note that this cannot be done for bindings (yet) because labels are not
    supplied as part of the binding request.
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/sqs/provision/main.tf
+++ terraform/sqs/provision/main.tf
@@ -1,7 +1,6 @@
 resource "aws_sqs_queue" "queue" {
   name       = var.fifo ? "${var.instance_name}.fifo" : var.instance_name
   fifo_queue = var.fifo
-  tags       = var.labels
 
   lifecycle {
     prevent_destroy = true
