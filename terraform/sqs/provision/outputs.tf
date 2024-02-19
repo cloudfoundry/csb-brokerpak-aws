fa67118991def9bb9119a7ab465496a315b601e1
George Blue <blgm@users.noreply.github.com>
2024-02-19 14:28:43 +0000

    feat: SQS provision status with URL and ARN (experimental) (#1540)
    The SQS provision status should contain the queue URL and ARN so that the user who created the queue can easily identify it. This output is visible from `cf service ...`.
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/sqs/provision/outputs.tf
+++ terraform/sqs/provision/outputs.tf
@@ -2,3 +2,10 @@ output "arn" { value = aws_sqs_queue.queue.arn }
 output "region" { value = var.region }
 output "queue_url" { value = aws_sqs_queue.queue.id }
 output "queue_name" { value = aws_sqs_queue.queue.name }
+output "status" {
+  value = format(
+    "created SQS queue: %s (ARN: %s)",
+    aws_sqs_queue.queue.id,
+    aws_sqs_queue.queue.arn
+  )
+}
