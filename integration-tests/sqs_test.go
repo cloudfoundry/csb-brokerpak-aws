7b5bf2f5db2e53019301d436fa1d34efb5b26f2f
Felisia Martini <fmartini@pivotal.io>
2024-02-20 15:46:32 +0000

    feat: Added config for visibility_timeout_seconds (#1554)
    * Added config for visibility_timeout_seconds
    
    * Fix formatting
    
    * Allow config for visibility_timeout_seconds
    
    * Allow config for max_message_size
    
    * Allow config for delaying messages
    
    * Allow config for receive_wait_time_seconds

--- integration-tests/sqs_test.go
+++ integration-tests/sqs_test.go
@@ -102,6 +102,11 @@ var _ = Describe("SQS", Label("SQS"), func() {
 					})),
 					HaveKeyWithValue("instance_name", fmt.Sprintf("csb-sqs-%s", instanceID)),
 					HaveKeyWithValue("fifo", BeFalse()),
+					HaveKeyWithValue("visibility_timeout_seconds", BeNumerically("==", 30)),
+					HaveKeyWithValue("message_retention_seconds", BeNumerically("==", 345600)),
+					HaveKeyWithValue("max_message_size", BeNumerically("==", 262144)),
+					HaveKeyWithValue("delay_seconds", BeZero()),
+					HaveKeyWithValue("receive_wait_time_seconds", BeZero()),
 					HaveKeyWithValue("region", fakeRegion),
 					HaveKeyWithValue("aws_access_key_id", awsAccessKeyID),
 					HaveKeyWithValue("aws_secret_access_key", awsSecretAccessKey),
@@ -112,12 +117,17 @@ var _ = Describe("SQS", Label("SQS"), func() {
 
 		It("should allow properties to be set on provision", func() {
 			_, err := broker.Provision(sqsServiceName, sqsCustomStandardPlanName, map[string]any{
-				"region":                "africa-north-4",
-				"fifo":                  true,
-				"aws_access_key_id":     "fake-aws-access-key-id",
-				"aws_secret_access_key": "fake-aws-secret-access-key",
-				"dlq_arn":               "fake-arn",
-				"max_receive_count":     5,
+				"region":                     "africa-north-4",
+				"fifo":                       true,
+				"visibility_timeout_seconds": 60,
+				"message_retention_seconds":  60,
+				"max_message_size":           1024,
+				"delay_seconds":              600,
+				"receive_wait_time_seconds":  20,
+				"aws_access_key_id":          "fake-aws-access-key-id",
+				"aws_secret_access_key":      "fake-aws-secret-access-key",
+				"dlq_arn":                    "fake-arn",
+				"max_receive_count":          5,
 			})
 			Expect(err).NotTo(HaveOccurred())
 
@@ -125,6 +135,11 @@ var _ = Describe("SQS", Label("SQS"), func() {
 				SatisfyAll(
 					HaveKeyWithValue("region", "africa-north-4"),
 					HaveKeyWithValue("fifo", BeTrue()),
+					HaveKeyWithValue("visibility_timeout_seconds", BeNumerically("==", 60)),
+					HaveKeyWithValue("message_retention_seconds", BeNumerically("==", 60)),
+					HaveKeyWithValue("max_message_size", BeNumerically("==", 1024)),
+					HaveKeyWithValue("delay_seconds", BeNumerically("==", 600)),
+					HaveKeyWithValue("receive_wait_time_seconds", BeNumerically("==", 20)),
 					HaveKeyWithValue("aws_access_key_id", "fake-aws-access-key-id"),
 					HaveKeyWithValue("aws_secret_access_key", "fake-aws-secret-access-key"),
 					HaveKeyWithValue("dlq_arn", "fake-arn"),
@@ -172,7 +187,11 @@ var _ = Describe("SQS", Label("SQS"), func() {
 			Entry(nil, "aws_secret_access_key", "fake-aws-secret-access-key"),
 			Entry(nil, "max_receive_count", 5),
 			Entry(nil, "dlq_arn", "fake-arn"),
-			Entry(nil, "aws_secret_access_key", "fake-aws-secret-access-key"),
+			Entry(nil, "visibility_timeout_seconds", 120),
+			Entry(nil, "message_retention_seconds", 60),
+			Entry(nil, "max_message_size", 1024),
+			Entry(nil, "delay_seconds", 300),
+			Entry(nil, "receive_wait_time_seconds", 15),
 		)
 	})
 
--- terraform/sqs/provision/main.tf
+++ terraform/sqs/provision/main.tf
@@ -1,6 +1,11 @@
 resource "aws_sqs_queue" "queue" {
-  name       = var.fifo ? "${var.instance_name}.fifo" : var.instance_name
-  fifo_queue = var.fifo
+  name                       = var.fifo ? "${var.instance_name}.fifo" : var.instance_name
+  fifo_queue                 = var.fifo
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
 
   redrive_policy = var.dlq_arn != "" ? jsonencode({
     deadLetterTargetArn = var.dlq_arn,
