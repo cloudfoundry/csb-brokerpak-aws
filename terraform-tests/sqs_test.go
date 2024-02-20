5475beeb843522174e4b5bc8c8775b18735862f5
Felisia Martini <fmartini@pivotal.io>
2024-02-20 16:04:04 +0000

    Fix formatting

--- terraform-tests/sqs_test.go
+++ terraform-tests/sqs_test.go
@@ -43,7 +43,7 @@ var _ = Describe("SQS", Label("SQS-terraform"), Ordered, func() {
 			"region":                     awsRegion,
 			"dlq_arn":                    "",
 			"max_receive_count":          5,
-      "dlq":                        false,
+			"dlq":                        false,
 		}
 	})
 
