4ff4b04be62c7c3cc3d1e2c1fc56f27fb617527e
Felisia Martini <fmartini@pivotal.io>
2024-03-13 15:44:11 +0000

    Update queue users permissions
    The same permissions are given to a Standard/FIFO queue user and to a DLQ user. We have no reasonn at the moment to think that the permissions should be different.
    If the instance has a DLQ associated the user will have permissions to trigger redrive from DLQ back to the original queue

--- terraform/sqs/bind/data.tf
+++ terraform/sqs/bind/data.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "user_policy" {
   dynamic "statement" {
-    for_each = local.final_policy
+    for_each = local.queue_policy
     content {
       sid       = try(statement.value.sid, null)
       actions   = try(statement.value.actions, null)
@@ -19,42 +19,24 @@ locals {
       "sqs:PurgeQueue",
       "sqs:GetQueueAttributes",
       "sqs:GetQueueUrl",
-      "sqs:ListQueueTags"
-    ],
-    resources : [var.arn]
-  }
-  dlq_access = {
-    sid : "DLQAccess",
-    actions : [
-      "sqs:ReceiveMessage",
-      "sqs:DeleteMessage",
-      "sqs:PurgeQueue",
-      "sqs:GetQueueAttributes",
-      "sqs:GetQueueUrl",
+      "sqs:ListQueues",
       "sqs:ListQueueTags",
-      "sqs:StartMessageMoveTask", // Can start a task but it needs SendMessage on the destination queue
-      "sqs:ListDeadLetterSourceQueues"
+      "sqs:ListDeadLetterSourceQueues",
     ],
     resources : [var.arn]
   }
-  standard_dql_access = {
+  dql_redrive_access = {
     sid : "sqsAccessDLQ",
     actions : [
-      "sqs:ReceiveMessage",
-      "sqs:DeleteMessage",
-      "sqs:GetQueueAttributes",
-      "sqs:GetQueueUrl",
-      "sqs:ListQueueTags",
       "sqs:StartMessageMoveTask",
       "sqs:CancelMessageMoveTask",
-      "sqs:ListMessageMoveTasks"
+      "sqs:ListMessageMoveTasks",
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
     ],
     resources : [var.dlq_arn]
   }
 
-  queue_policy_statements = var.dlq ? [local.dlq_access] : [local.standard_access]
-
-  add_dql_access = concat([local.standard_access], [local.standard_dql_access])
-
-  final_policy = length(var.dlq_arn) > 0 ? local.add_dql_access : local.queue_policy_statements
+  queue_policy = length(var.dlq_arn) > 0 ? concat([local.standard_access], [local.dql_redrive_access]) : [ local.standard_access ]
 }
