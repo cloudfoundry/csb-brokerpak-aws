80a79b533b4abe038f050ad35044edc0edbbf17b
Andrea Zucchini <zandrea@vmware.com>
2024-03-15 11:59:54 +0000

    feat: Server-Side Encryption for SQS Queues (#1619)
    * feat: Server-Side Encryption for SQS Queues
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * fix: description must be in accordance with the code
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * test: version is not necessary
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * feat: KMS support
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * chore: remove unnecessary comment
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * chore: keep variables closer to where they are used
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * chore: use last version functionality of CF CLI
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * chore: apply correct format
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)
    
    * test: delete unnecessary env variable
    
    [#187061621](https://www.pivotaltracker.com/story/show/187061621)

--- terraform/sqs/provision/variables.tf
+++ terraform/sqs/provision/variables.tf
@@ -21,4 +21,7 @@ variable "max_receive_count" { type = number }
 variable "content_based_deduplication" { type = bool }
 variable "deduplication_scope" { type = string }
 variable "fifo_throughput_limit" { type = string }
-
+variable "sqs_managed_sse_enabled" { type = bool }
+variable "kms_master_key_id" { type = string }
+variable "kms_data_key_reuse_period_seconds" { type = number }
+variable "kms_extra_key_ids" { type = string }
