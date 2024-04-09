29006f3b76b39f14cf69c4ea11c6fdce583abee5
Felisia Martini <fmartini@pivotal.io>
2024-04-09 18:18:11 +0100

    fix: Include random provider for redis (#1728)

--- terraform/redis/cluster/provision/versions.tf
+++ terraform/redis/cluster/provision/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "registry.terraform.io/hashicorp/aws"
       version = ">= 5.0"
     }
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = ">= 3.3.2"
+    }
   }
 }
