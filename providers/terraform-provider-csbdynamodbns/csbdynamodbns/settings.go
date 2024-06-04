d882ea74d32022fee9725d382fde08c9796b6da7
George Blue <gblue@pivotal.io>
2024-06-04 19:00:19 +0100

    chore: common DynamoDB client builder
    To reduce complexity, the DynamoDB client creation code has been
    factored out.
    
    [#187733068](https://www.pivotaltracker.com/story/show/187733068)

--- providers/terraform-provider-csbdynamodbns/csbdynamodbns/settings.go
+++ providers/terraform-provider-csbdynamodbns/csbdynamodbns/settings.go
@@ -2,28 +2,14 @@ package csbdynamodbns
 
 import (
 	"context"
-	"net/url"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	smithyendpoints "github.com/aws/smithy-go/endpoints"
+	"github.com/cloudfoundry/csb-brokerpak-aws/terraform-provider-dynamodbns/dynaclient"
 )
 
 //counterfeiter:generate -header csbdynamodbnsfakes/header.txt . DynamoDBConfig
 type DynamoDBConfig interface {
 	GetClient(ctx context.Context, keyID, secretKey string) (DynamoDBClient, error)
 	GetPrefix() string
-}
-
-type endpointResolverV2Func func(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error)
-
-// Fail fast if the interface is not implemented
-var _ dynamodb.EndpointResolverV2 = endpointResolverV2Func(nil)
-
-func (e endpointResolverV2Func) ResolveEndpoint(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error) {
-	return e(ctx, params)
 }
 
 type dynamoDBNamespaceSettings struct {
@@ -40,34 +26,5 @@ func (d *dynamoDBNamespaceSettings) GetPrefix() string {
 }
 
 func (d *dynamoDBNamespaceSettings) GetClient(ctx context.Context, keyID, secretKey string) (DynamoDBClient, error) {
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion(d.region),
-		config.WithCredentialsProvider(
-			aws.NewCredentialsCache(
-				credentials.NewStaticCredentialsProvider(
-					keyID,
-					secretKey,
-					"",
-				),
-			),
-		),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// For testing we use a custom endpoint
-	var opts []func(*dynamodb.Options)
-	if d.customEndpointURL != "" {
-		u, err := url.Parse(d.customEndpointURL)
-		if err != nil {
-			return nil, err
-		}
-
-		opts = append(opts, dynamodb.WithEndpointResolverV2(endpointResolverV2Func(func(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error) {
-			return smithyendpoints.Endpoint{URI: *u}, nil
-		})))
-	}
-
-	return dynamodb.NewFromConfig(cfg, opts...), nil
+	return dynaclient.New(ctx, d.region, keyID, secretKey, d.customEndpointURL)
 }
