7d32b1be60d8781cfa663b16df707c6ab2eb0e76
George Blue <blgm@users.noreply.github.com>
2024-06-04 17:25:00 +0100

    chore: refactor DynamoDB test endpoint (#1873)
    `aws.EndpointResolverWithOptionsFunc` and `aws.Endpoint` are deprecated
    in github.com/aws/aws-sdk-go-v2 v1.27.1, so let's update to the
    replacement dynamodb.EndpointResolverV2.
    
    [#187733068](https://www.pivotaltracker.com/story/show/187733068)

--- providers/terraform-provider-csbdynamodbns/csbdynamodbns/settings.go
+++ providers/terraform-provider-csbdynamodbns/csbdynamodbns/settings.go
@@ -2,18 +2,14 @@ package csbdynamodbns
 
 import (
 	"context"
+	"net/url"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	smithyendpoints "github.com/aws/smithy-go/endpoints"
 )
-
-type dynamoDBNamespaceSettings struct {
-	region            string
-	prefix            string
-	customEndpointURL string
-}
 
 //counterfeiter:generate -header csbdynamodbnsfakes/header.txt . DynamoDBConfig
 type DynamoDBConfig interface {
@@ -21,6 +17,22 @@ type DynamoDBConfig interface {
 	GetPrefix() string
 }
 
+type endpointResolverV2Func func(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error)
+
+// Fail fast if the interface is not implemented
+var _ dynamodb.EndpointResolverV2 = endpointResolverV2Func(nil)
+
+func (e endpointResolverV2Func) ResolveEndpoint(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error) {
+	return e(ctx, params)
+}
+
+type dynamoDBNamespaceSettings struct {
+	region            string
+	prefix            string
+	customEndpointURL string
+}
+
+// Fail fast if the interface is not implemented
 var _ DynamoDBConfig = &dynamoDBNamespaceSettings{}
 
 func (d *dynamoDBNamespaceSettings) GetPrefix() string {
@@ -28,44 +40,34 @@ func (d *dynamoDBNamespaceSettings) GetPrefix() string {
 }
 
 func (d *dynamoDBNamespaceSettings) GetClient(ctx context.Context, keyID, secretKey string) (DynamoDBClient, error) {
-	optFns := []func(options *config.LoadOptions) error{
+	cfg, err := config.LoadDefaultConfig(ctx,
 		config.WithRegion(d.region),
-	}
-	if d.customEndpointURL == "" {
-		optFns = append(optFns,
-			config.WithCredentialsProvider(
-				aws.NewCredentialsCache(
-					credentials.NewStaticCredentialsProvider(
-						keyID,
-						secretKey,
-						"",
-					),
+		config.WithCredentialsProvider(
+			aws.NewCredentialsCache(
+				credentials.NewStaticCredentialsProvider(
+					keyID,
+					secretKey,
+					"",
 				),
 			),
-		)
-	} else {
-		optFns = append(optFns,
-			config.WithEndpointResolverWithOptions(
-				aws.EndpointResolverWithOptionsFunc(
-					func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-						return aws.Endpoint{URL: d.customEndpointURL}, nil
-					},
-				),
-			),
-			config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
-				Value: aws.Credentials{
-					AccessKeyID:     "dummy",
-					SecretAccessKey: "dummy",
-					SessionToken:    "dummy",
-					Source:          "Hard-coded credentials; the values are irrelevant",
-				},
-			}),
-		)
-	}
-	cfg, err := config.LoadDefaultConfig(ctx, optFns...)
+		),
+	)
 	if err != nil {
 		return nil, err
 	}
 
-	return dynamodb.NewFromConfig(cfg), nil
+	// For testing we use a custom endpoint
+	var opts []func(*dynamodb.Options)
+	if d.customEndpointURL != "" {
+		u, err := url.Parse(d.customEndpointURL)
+		if err != nil {
+			return nil, err
+		}
+
+		opts = append(opts, dynamodb.WithEndpointResolverV2(endpointResolverV2Func(func(ctx context.Context, params dynamodb.EndpointParameters) (smithyendpoints.Endpoint, error) {
+			return smithyendpoints.Endpoint{URI: *u}, nil
+		})))
+	}
+
+	return dynamodb.NewFromConfig(cfg, opts...), nil
 }
