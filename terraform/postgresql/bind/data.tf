f8af4e37fd82f946ffeaef6be5b70b6cf93baaf3
Felisia Martini <fmartini@vmware.com>
2024-10-09 15:38:17 +0100

    Fix binding opeeration for both password and managed secret cases
    A password instance can now be updated to a managed secret instance

--- terraform/postgresql/bind/data.tf
+++ terraform/postgresql/bind/data.tf
@@ -27,11 +27,11 @@ resource "random_password" "password" {
 }
 
 data "aws_secretsmanager_secret_version" "secret-version" {
-  # count = var.use_managed_admin_password ? 1 : 0
+  count     = var.use_managed_admin_password ? 1 : 0
   secret_id = var.master_secret_arn
 }
 
 locals {
-  managed_admin_creds = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)
-  managed_admin_password = local.managed_admin_creds.password
+  managed_admin_creds    = var.use_managed_admin_password ? jsondecode(data.aws_secretsmanager_secret_version.secret-version[0].secret_string) : {}
+  managed_admin_password = var.use_managed_admin_password ? local.managed_admin_creds.password : ""
 }
