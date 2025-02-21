e5ed44ed75df7a186ef5fd129e292e121c4ad543
George Blue <blgm@users.noreply.github.com>
2025-02-21 11:17:12 +0000

    test: restore of RDS using secrets manager (#2409)

--- acceptance-tests/helpers/awscli/aws.go
+++ acceptance-tests/helpers/awscli/aws.go
@@ -13,14 +13,19 @@ import (
 )
 
 func AWS(args ...string) []byte {
-	cmd := exec.Command("aws", args...)
-	session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
-	_, _ = fmt.Fprintf(ginkgo.GinkgoWriter, "Running: %s\n", cmd.String())
-	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	session := AWSSession(args...)
 	gomega.Eventually(session).WithTimeout(time.Minute).Should(gexec.Exit(0))
 	return session.Out.Contents()
 }
 
 func AWSToJSON[R any, pR *R](receiver pR, args ...string) {
 	gomega.Expect(jsonry.Unmarshal(AWS(args...), receiver)).NotTo(gomega.HaveOccurred())
+}
+
+func AWSSession(args ...string) *gexec.Session {
+	cmd := exec.Command("aws", args...)
+	session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
+	_, _ = fmt.Fprintf(ginkgo.GinkgoWriter, "Running: %s\n", cmd.String())
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	return session
 }
