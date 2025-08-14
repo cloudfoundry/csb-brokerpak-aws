ab9801eb3c23f15abbdc0c62cde6577c68b66669
George Blue <blgm@users.noreply.github.com>
2025-08-14 12:35:57 +0100

    chore: OpenTofu v1.10 and AWS provider v6 (#2630)

--- terraform/dynamodb-namespace/provision/versions.tf
+++ terraform/dynamodb-namespace/provision/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 5"
+      version = "~> 6"
     }
   }
 }
--- terraform/sqs/bind/versions.tf
+++ terraform/sqs/bind/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 5"
+      version = "~> 6"
     }
   }
 }
