0ddd0f416e77a7f9a711997a36f910cb2bc1c626
George Blue <george.blue@broadcom.com>
2025-09-08 17:18:54 +0100

    chore: apply fixes from Go modernize command
    The modernize command replaces old constructs with simpler, updated ones.
    
    Command is:
    go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest -fix -test ./...

--- terraform-tests/helpers/slice_filters_generic.go
+++ terraform-tests/helpers/slice_filters_generic.go
@@ -1,14 +1,10 @@
 package helpers
 
+import "slices"
+
 func Filter[T comparable](existingElements []T, elementsToFilter ...T) (filteredElements []T) {
 	for _, curElement := range existingElements {
-		hasToBeFiltered := false
-		for _, elementToFilter := range elementsToFilter {
-			if curElement == elementToFilter {
-				hasToBeFiltered = true
-				break
-			}
-		}
+		hasToBeFiltered := slices.Contains(elementsToFilter, curElement)
 		if !hasToBeFiltered {
 			filteredElements = append(filteredElements, curElement)
 		}
